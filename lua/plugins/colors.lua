866a86e88f1331717c08c14720509f31a174cc3e
JD <126777940+JDl337@users.noreply.github.com>
2023-05-18 12:12:33 +0530

    Finally settle on a color scheme
    Good ol gruvbox material, can't find an adequate lua replacement

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,8 @@
+return {
+  "sainnhe/gruvbox-material",
+  config = function()
+    vim.g.gruvbox_material_background = 'soft'
+    vim.g.gruvbox_material_better_performance = 1
+    vim.cmd('colorscheme gruvbox-material')
+  end,
+}
