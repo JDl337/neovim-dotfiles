808519e02f44270a6c4f54e9957d00b89eb99bb2
JD <126777940+JDl337@users.noreply.github.com>
2023-05-25 19:05:59 +0530

    Add toggleterm and config

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    --[[ things you want to change go here]]
+    open_mapping = [[<C-\>]],
+    insert_mappings = true,
+    shade_terminals = false,
+    direction = 'vertical',
+    size = 80,
+  },
+}
